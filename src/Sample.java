4af836d1e5d79a6e7b8682029ecf3013dd91940f
sagar <sagarslife222@gmail.com>
2025-09-14 15:40:59 +0530

    added hi method

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,10 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        sayHi();
+    }
+
+    static void sayHi() {
+        System.out.println("HIIIII");
     }
 }
